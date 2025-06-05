20898fce7987027b4dd499554f9f2e0f34934591
Louis La Grange <louis@louislagrange.co.za>
2025-06-05 22:20:04 +0200

    Update Joke example with non-explicit value

--- src/app/models/joke.model.ts
+++ src/app/models/joke.model.ts
@@ -3,14 +3,13 @@
  * @example An example joke
  * ```json
  * {
- *  "categories": [ "explicit" ],
- *  "created_at": "2020-01-05 13:42:20.568859",
- *  "icon_url": "https://api.chucknorris.io/img/avatar/chuck-norris.png",
- *  "id": "vyF8HL08TeeqUaTqm-v7xA",
- *  "updated_at": "2020-01-05 13:42:20.568859",
- *  "url": "https://api.chucknorris.io/jokes/vyF8HL08TeeqUaTqm-v7xA",
- *  "value": "Chuck Norris once had sex with a dinosaur and then they became extinct."
- * }
+ * "categories": ["movie"],
+ * "created_at": "2020-01-05 13:42:29.855523",
+ * "icon_url": "https://api.chucknorris.io/img/avatar/chuck-norris.png",
+ * "id": "0JqamoTkR_GK0DYbigQ5cw",
+ * "updated_at": "2020-01-05 13:42:29.855523",
+ * "url": "https://api.chucknorris.io/jokes/0JqamoTkR_GK0DYbigQ5cw",
+ * "value": "Arnold Schwarzenegger always says he'll be back. But Chuck Norris always handles things the first time"}
  * ```
  *
  */
