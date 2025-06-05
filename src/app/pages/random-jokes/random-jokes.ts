7d9fc5264517ab36cd9d08430b03238bb3744595
Louis La Grange <louis@louislagrange.co.za>
2025-06-05 21:43:47 +0200

    Fetch 10 jokes and display them neatly

--- src/app/pages/random-jokes/random-jokes.ts
+++ src/app/pages/random-jokes/random-jokes.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { JokeService } from '../../services/joke-service';
+import { Joke } from '../../models/joke.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-random-jokes',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './random-jokes.html',
-  styleUrl: './random-jokes.scss'
+  styleUrl: './random-jokes.scss',
 })
-export class RandomJokes {
+export class RandomJokes implements OnInit {
+  public jokes = signal<Joke[]>([]);
 
+  constructor(private readonly jokeService: JokeService) {
+    console.log('fetching jokes');
+  }
+
+  public ngOnInit() {
+    this.jokeService.fetchJokes(10).subscribe({
+      next: (joke) => {
+        this.jokes.update((previous) => [...previous, joke]);
+      },
+      error: (err) => {
+        console.warn(err);
+        // TODO: Error handling
+      },
+    });
+  }
 }
