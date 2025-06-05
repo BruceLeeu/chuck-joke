2ae57f04971f78009cda8387a65bbe79ae0d01af
Louis La Grange <louis@louislagrange.co.za>
2025-06-05 21:45:43 +0200

    Implement a rudementary 5s timer to replace a joke

--- src/app/pages/random-jokes/random-jokes.ts
+++ src/app/pages/random-jokes/random-jokes.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, signal } from '@angular/core';
 import { JokeService } from '../../services/joke-service';
 import { Joke } from '../../models/joke.model';
 import { DatePipe } from '@angular/common';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-random-jokes',
@@ -11,13 +12,36 @@ import { DatePipe } from '@angular/common';
 })
 export class RandomJokes implements OnInit {
   public jokes = signal<Joke[]>([]);
+  public timerActive = signal(false);
 
-  constructor(private readonly jokeService: JokeService) {
-    console.log('fetching jokes');
-  }
+  private timer?: number;
+
+  constructor(private readonly jokeService: JokeService) {}
 
   public ngOnInit() {
-    this.jokeService.fetchJokes(10).subscribe({
+    this.processJokes(this.jokeService.fetchJokes(10));
+  }
+
+  public toggleTimer() {
+    this.timerActive.update((active) => {
+      if (active) {
+        clearInterval(this.timer);
+        this.timer = undefined;
+      } else {
+        this.timer = setInterval(() => {
+          this.jokes.update((previous) => {
+            previous.shift();
+            return previous;
+          });
+          this.processJokes(this.jokeService.fetchJokes(1));
+        }, 5000);
+      }
+      return !active;
+    });
+  }
+
+  public processJokes(jokeObservable: Observable<Joke>) {
+    jokeObservable.subscribe({
       next: (joke) => {
         this.jokes.update((previous) => [...previous, joke]);
       },
