8ad113a003cd50000c28338eff25ca33981781cb
Louis La Grange <louis@louislagrange.co.za>
2025-06-06 14:54:28 +0200

    Fix broken ng test command to test GH action

--- src/app/pages/random-jokes/random-jokes.spec.ts
+++ src/app/pages/random-jokes/random-jokes.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RandomJokes } from './random-jokes';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('RandomJokes', () => {
   let component: RandomJokes;
@@ -8,9 +10,9 @@ describe('RandomJokes', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RandomJokes]
-    })
-    .compileComponents();
+      imports: [RandomJokes],
+      providers: [provideHttpClientTesting(), provideHttpClient()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RandomJokes);
     component = fixture.componentInstance;
--- src/app/services/joke-service.spec.ts
+++ src/app/services/joke-service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { JokeService } from './joke-service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('JokeService', () => {
   let service: JokeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClientTesting(), provideHttpClient()],
+    });
     service = TestBed.inject(JokeService);
   });
 
