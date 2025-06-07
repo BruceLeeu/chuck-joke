37eeceeb47a94dbbebe1062f7d9028ff4c313e26
Louis La Grange <louis@louislagrange.co.za>
2025-06-07 12:51:24 +0200

    Add navbar with routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navigation } from './components/navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'chuck-joke';
+  protected title = 'Chuck Joke';
 }
